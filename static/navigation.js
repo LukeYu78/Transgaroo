973d48c76c06e47a6663f50ec2fa6e1e3b953751
Girish <girish.bhatta@alexsolutions.com.au>
2019-08-29 03:41:25 +1000

    Final changes for the demo

--- static/navigation.js
+++ static/navigation.js
@@ -1,9 +1,11 @@
 
-const accessToken = 'pk.eyJ1IjoiZ2lyaXNoMSIsImEiOiJjanp2NGIwcmkwaTRjM21tdnN5NTlqb3NqIn0.isvbmWcUY3d25ip9294Igg';
+ 
 
 
 
 $(document).ready(function() { 
+
+    const accessToken = 'pk.eyJ1IjoiZ2lyaXNoMSIsImEiOiJjanp2NGIwcmkwaTRjM21tdnN5NTlqb3NqIn0.isvbmWcUY3d25ip9294Igg';
 
     mapboxgl.accessToken = accessToken;
     var map = new mapboxgl.Map({
@@ -13,14 +15,26 @@ $(document).ready(function() {
         zoom: 13
         });
 
- 
+
     var geocoder = new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
         mapboxgl: mapboxgl
         });
 
     document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
-     
+
+   
+    
+
+    // each
+    // function plotOnMap(success){
+    //   map.on('load',function(){
+    //     success.forEach((eachItem)=>{
+    //       map.addLayer(eachItem);
+    //     })
+    //   })
+    // }
+   
     $(document).on('click', '#search', function(e) {
         let encodedValue = encodeURIComponent($(".mapboxgl-ctrl-geocoder--input").val())
         console.log(encodedValue);
@@ -33,7 +47,6 @@ $(document).ready(function() {
         var requestJson = new Object();
         var coordinates = new Object();
         
-
         coordinates.lat = data.features[0].center[0];
         coordinates.long = data.features[0].center[1];
 
@@ -43,7 +56,44 @@ $(document).ready(function() {
             coordinates.isParkingBay = "False";
         }
         requestJson.coordinates = coordinates;
-        sendRequestToServer(requestJson)
+        sendRequestToServer(requestJson).then((success)=>{
+          // if(requestJson.isParkingBay == "False"){
+          //   success.forEach((item)=>{
+          //     map.addLayer(item);
+          //   });
+          // }else{
+          //   success.forEach((item)=>{
+          //     map.loadImage('./parkingImage.png', function(error, image){
+          //       if (error) throw error;
+          //       map.addImage('parkingBay',image);
+          //       map.addLayer(item);
+          //     });
+          //   });
+          // }
+
+          success.forEach((item)=>{
+            if(item.type == "FeatureCollection"){
+              // map.loadImage('/static/parkingImage.png', function(error, image){
+              //     if (error) throw error;
+              //     map.addImage('parkingBay',image);
+              //     map.addLayer(item);
+              // });
+              item.features.forEach((marker)=>{
+                var el = document.createElement('div');
+                el.className = 'marker';
+
+                new mapboxgl.Marker(el)
+                    .setLngLat(marker.geometry.coordinates)
+                    .addTo(map);
+              });
+            }else{
+              map.addLayer(item);
+            }
+          })
+          
+        },(err)=>{
+          console.log(err);
+        });
       })
       .fail(function(err) {
         console.log("error"+JSON.stringify(err))
@@ -53,18 +103,121 @@ $(document).ready(function() {
 
 
 function sendRequestToServer(requestJson){
+  return new Promise((resolve,reject)=> {
     $.ajax({
-        url: 'getAllCyclePaths',
-        type: 'POST',
-        dataType: 'json',
-        data: JSON.stringify(requestJson),
-        contentType:"application/json; charset=UTF-8"
-      })
-      .done(function(response) {
-        console.log("data was recieved" + JSON.stringify(response));
-        //prepareDataForPlot(response)
-      })
-      .fail(function(err) {
-        console.log("error"+err)
-      })
+      url: 'getAllCyclePaths',
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(requestJson),
+      contentType:"application/json; charset=UTF-8"
+    })
+    .done(function(response) {
+      if(requestJson.coordinates["isParkingBay"] === "False"){
+        var transformedData = preparedataforMap(response);
+        resolve(transformedData);
+      }else{
+        var transformedPointData = prepareDataForParking(response);
+        resolve(transformedPointData);
+      }
+      
+    })
+    .fail(function(err) {
+      console.log("error"+err);
+      reject(err);
+    })
+  })
+}
+
+
+
+function preparedataforMap(response){
+  var multlineData = new Array();
+  
+  response.forEach((element) => {
+    var eachLine = new Object();
+    
+    var source = new Object();
+    var data = new Object();
+    var properties = new Object();
+    var geometry = new Object();
+    var layout = new Object();
+    var paint = new Object();
+
+
+
+    geometry.type = "LineString";
+    geometry.coordinates = element.geom_data[0];
+
+    data.type = "Feature";
+    data.properties = properties;
+    data.geometry = geometry;
+
+    source.type = "geojson";
+    source.data = data;
+
+    
+    layout["line-join"] = "round";
+    layout["line-cap"]= "round";
+
+    paint["line-color"] = "#6600ff";
+    paint["line-width"] = 6;
+
+    eachLine.id = "route"+element.id.toString();
+    eachLine.type = "line";
+    eachLine.source = source;
+    eachLine.layout = layout;
+    eachLine.paint = paint;
+
+    multlineData.push(eachLine);
+  });
+
+  return multlineData;
+}
+
+
+
+function prepareDataForParking(response){
+  var multiPointData = new Array();
+  
+  response.forEach((element) => {
+    var overallObj = new Object();
+    var features = new Array();
+    element.parkingBays.forEach((eachParkingBay)=>{
+      var properties = new Object();
+      var geometry = new Object();
+      var innerObj = new Object();
+
+      geometry.coordinates = eachParkingBay.coordinates;
+      geometry.type = "Point";
+
+      properties.title = "parkingBayLocation";
+      properties.description = "CityOFMelbourne";
+
+      innerObj.type = "Feature";
+      innerObj.geometry = geometry;
+      innerObj.properties = properties; 
+
+      features.push(innerObj);
+    })
+    overallObj.type = "FeatureCollection";
+    overallObj.features = features;
+
+    // data.features = features;
+    // data.type = "FeatureCollection";
+
+    // source.data = data;
+    // source.type = "geojson";
+
+    // overallObj.source = source;
+    // overallObj.id = "parkingBay"+element.id.toString();
+    // overallObj.type = "symbol";
+
+    // overallObj["icon-image"] = "parkingBay";
+    // overallObj["icon-size"] = 0.1;
+
+    // multiPointData.push(overallObj);
+    multiPointData.push(overallObj);
+   });
+
+  return multiPointData;
 }
