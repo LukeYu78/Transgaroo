54412d14b10eec7c17e8a24474b032f106b1c952
Girish <girish.bhatta@alexsolutions.com.au>
2019-10-04 00:15:43 +1000

    WIP :adding the planning icoan

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -42,6 +42,62 @@ $(document).ready(function() {
     mapboxgl: mapboxgl
   });
 
+  var draw = new MapboxDraw({
+    // Instead of showing all the draw tools, show only the line string and delete tools
+    displayControlsDefault: false,
+    controls: {
+      line_string: true,
+      trash: true
+    },
+    styles: [
+      // Set the line style for the user-input coordinates
+      {
+        "id": "gl-draw-line",
+        "type": "line",
+        "filter": ["all", ["==", "$type", "LineString"],
+          ["!=", "mode", "static"]
+        ],
+        "layout": {
+          "line-cap": "round",
+          "line-join": "round"
+        },
+        "paint": {
+          "line-color": "#438EE4",
+          "line-dasharray": [0.2, 2],
+          "line-width": 4,
+          "line-opacity": 0.7
+        }
+      },
+      // Style the vertex point halos
+      {
+        "id": "gl-draw-polygon-and-line-vertex-halo-active",
+        "type": "circle",
+        "filter": ["all", ["==", "meta", "vertex"],
+          ["==", "$type", "Point"],
+          ["!=", "mode", "static"]
+        ],
+        "paint": {
+          "circle-radius": 12,
+          "circle-color": "#FFF"
+        }
+      },
+      // Style the vertex points
+      {
+        "id": "gl-draw-polygon-and-line-vertex-active",
+        "type": "circle",
+        "filter": ["all", ["==", "meta", "vertex"],
+          ["==", "$type", "Point"],
+          ["!=", "mode", "static"]
+        ],
+        "paint": {
+          "circle-radius": 8,
+          "circle-color": "#438EE4",
+        }
+      },
+    ]
+  });
+
+  map.addControl(draw);
   document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
   document.getElementById('dest_geocoder').appendChild(dest_geocoder.onAdd(map));
   
