63318b4a0fdad0e3be0e54c1da3e283638f8d81c
Girish <girish.bhatta@alexsolutions.com.au>
2019-10-04 17:10:49 +1000

    integrating the map mathcing api for all the points selected.

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -97,6 +97,137 @@ $(document).ready(function() {
     ]
   });
 
+  function updateRoute() {
+    // Set the profile
+    var profile = "cycling";
+    // Get the coordinates that were drawn on the map
+    var data = draw.getAll();
+    var lastFeature = data.features.length - 1;
+    var coords = data.features[lastFeature].geometry.coordinates;
+    // Format the coordinates
+    var newCoords = coords.join(';')
+    // Set the radius for each coordinate pair to 25 meters
+    var radius = [];
+    coords.forEach(element => {
+      radius.push(25);
+    });
+    getMatch(newCoords, radius, profile);
+  }
+  
+  // Make a Map Matching request
+  function getMatch(coordinates, radius, profile) {
+    // Separate the radiuses with semicolons
+    var radiuses = radius.join(';')
+    // Create the query
+    var query = 'https://api.mapbox.com/matching/v5/mapbox/' + profile + '/' + coordinates + '?geometries=geojson&radiuses=' + radiuses + '&steps=true&access_token=' + mapboxgl.accessToken;
+  
+    $.ajax({
+      method: 'GET',
+      url: query
+    }).done(function(data) {
+      // Get the coordinates from the response
+      var coords = data.matchings[0].geometry;
+      console.log(coords);
+      // Code from the next step will go here
+    });
+  }
+
+  // Draw the Map Matching route as a new layer on the map
+  function addRoute(coords) {
+    // If a route is already loaded, remove it
+    if (map.getSource('route')) {
+      map.removeLayer('route')
+      map.removeSource('route')
+    } else { // Add a new layer to the map
+      map.addLayer({
+        "id": "route",
+        "type": "line",
+        "source": {
+          "type": "geojson",
+          "data": {
+            "type": "Feature",
+            "properties": {},
+            "geometry": coords
+          }
+        },
+        "layout": {
+          "line-join": "round",
+          "line-cap": "round"
+        },
+        "paint": {
+          "line-color": "#03AA46",
+          "line-width": 8,
+          "line-opacity": 0.8
+        }
+      });
+    };
+  }
+
+  // Make a Map Matching request
+  function getMatch(coordinates, radius, profile) {
+    // Separate the radiuses with semicolons
+    var radiuses = radius.join(';')
+    // Create the query
+    var query = 'https://api.mapbox.com/matching/v5/mapbox/' + profile + '/' + coordinates + '?geometries=geojson&radiuses=' + radiuses + '&steps=true&access_token=' + mapboxgl.accessToken;
+    console.log(query)
+    $.ajax({
+      method: 'GET',
+      url: query
+    }).done(function(data) {
+      // Get the coordinates from the response
+      var coords = data.matchings[0].geometry;
+      // Draw the route on the map
+      addRoute(coords);
+    });
+  }
+
+  function getInstructions(data) {
+    // Target the sidebar to add the instructions
+    var directions = document.getElementById('directions');
+  
+    var legs = data.legs;
+    var tripDirections = [];
+    // Output the instructions for each step of each leg in the response object
+    for (var i = 0; i < legs.length; i++) {
+      var steps = legs[i].steps;
+      for (var j = 0; j < steps.length; j++) {
+        tripDirections.push('<br><li>' + steps[j].maneuver.instruction) + '</li>';
+      }
+    }
+    directions.innerHTML = '<br><h2>Trip duration: ' + Math.floor(data.duration / 60) + ' min.</h2>' + tripDirections;
+  }
+
+  // Make a Map Matching request
+  function getMatch(coordinates, radius, profile) {
+    // Separate the radiuses with semicolons
+    var radiuses = radius.join(';')
+    // Create the query
+    var query = 'https://api.mapbox.com/matching/v5/mapbox/' + profile + '/' + coordinates + '?geometries=geojson&radiuses=' + radiuses + '&steps=true&access_token=' + mapboxgl.accessToken;
+    console.log(query)
+    $.ajax({
+      method: 'GET',
+      url: query
+    }).done(function(data) {
+      var coords = data.matchings[0].geometry;
+      // Draw the route on the map
+      addRoute(coords);
+      getInstructions(data.matchings[0]);
+    });
+  }
+
+  function removeRoute() {
+    if (map.getSource('route')) {
+      map.removeLayer('route');
+      map.removeSource('route');
+    } else {
+      return;
+    }
+  }
+    
+  map.on('draw.create', updateRoute);
+  map.on('draw.update', updateRoute);
+  map.on('draw.delete', removeRoute);
+
   map.addControl(draw);
   document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
   document.getElementById('dest_geocoder').appendChild(dest_geocoder.onAdd(map));
