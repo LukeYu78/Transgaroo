99317bfc38a1010112d42948c0db8995792aba6b
Girish <girish.bhatta@alexsolutions.com.au>
2019-10-20 01:55:23 +1100

    adding evolution files for the dtabase setup for the application

--- controllers/QueryController.py
+++ controllers/QueryController.py
@@ -14,7 +14,7 @@ class WanderingRoute:
     conn = None
     
     def __init__(self):
-       self.conn = psycopg2.connect("host=localhost dbname=IE-database user=postgres port=5433")
+       self.conn = psycopg2.connect("host=localhost dbname=IE-database user=postgres")
        self.cur = self.conn.cursor()
        self.isWandering = True
         
